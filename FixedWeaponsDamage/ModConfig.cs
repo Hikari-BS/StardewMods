7761875acf25106802b7dd28bf075899f2c9085a
Afif Shafara <afifshafara445566@gmail.com>
2024-01-16 16:30:07 +0700

    Add project files.

---
+++ FixedWeaponsDamage/ModConfig.cs
@@ -0,0 +1,36 @@
+﻿namespace FixedWeaponsDamage
+{
+    /// <summary>
+    /// Configurations for the mod.
+    /// </summary>
+    internal class ModConfig
+    {
+        private int percentMeleeDamageMultiplier = 100;
+        private int percentSlingshotDamageMultiplier = 100;
+
+        public bool EnableMeleeFixedDamage { get; set; } = true;
+        public int PercentMeleeDamageMultiplier
+        {
+            get => percentMeleeDamageMultiplier;
+
+            // evaluate so that curious player won't be able to assign negative number and crash the game (idk though, probably not XD)
+            // also set it to 1000 if it's greater than 1000
+            set
+            {
+                value = value < 0 ? 0 : value > 1000 ? 1000 : value; // ternary 4life lol
+                percentMeleeDamageMultiplier = value;
+            }
+        }
+
+        public bool EnableSlingshotFixedDamage { get; set; } = true;
+        public int PercentSlingshotDamageMultiplier
+        {
+            get => percentSlingshotDamageMultiplier;
+            set
+            {
+                value = value < 0 ? 0 : value > 1000 ? 1000 : value;
+                percentSlingshotDamageMultiplier = value;
+            }
+        }
+    }
+}
